dcfe109a293489df5bd53acb2b99fc98f22c96af
Lucas Lefrancq <lucas.lefrancq@hotmail.fr>
2020-11-25 13:23:35 +0100

    modif readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# 42_Get_next_line
+# Get_next_line (@42Paris)
+
+"Reading a line on a fd is way too tedious"
+
+![Alt text](https://github.com/lucaslefrancq/42_get_next_line/blob/master/get_next_line_example.png)
+
+## About
+
+Get_next_line is a project of the mandatory part of the cursus mainly code in C.
+It's about file descriptors, read and use of buffers. This get_next_line handle
+the bonus part, so it's able to read several files from multiple fds at the same
+time using a chainlist.
+
+- Here is the [subject][1]
+
+`This project was code on MacOS`
+
+### Building and running the project
+
+1. Download/Clone this repo
+
+        git clone https://github.com/lucaslefrancq/42_get_next_line
+
+2. Copy your main.c inside the root directory and `cd` into it. Then compile your main with the C files.
+	You need to indicates the size of the buffer that's get_next_line is using.
+
+        cd 42_Get_next_line
+		gcc -Wall -Werror -Wextra -D BUFFER_SIZE=10 main.c get_next_line.c get_next_line_utils.c
+
+[1]: https://github.com/lucaslefrancq/42_get_next_line/blob/master/get_next_line.en.subject.pdf
---
+++ get_next_line.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucaslefrancq <lucaslefrancq@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/28 18:01:42 by llefranc          #+#    #+#             */
+/*   Updated: 2020/11/25 12:11:12 by lucaslefran      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+typedef struct		s_list
+{
+	int				fd;
+	char			*content;
+	struct s_list	*next;
+}					t_list;
+
+void				*ft_memset(void *b, int c, size_t len);
+int					initchain(t_list **first, t_list *fdelem,
+							char **line, int fd);
+int					findline(t_list *elem, char **line, size_t len);
+int					get_next_line(int fd, char **line);
+void				ft_delone(t_list **first, t_list *elem);
+
+void				ft_lstadd_back(t_list **alst, t_list *new);
+t_list				*ft_lstnew(int fd, char *content);
+char				*ft_strdup(char *s1, size_t len);
+char				*ft_strjoin(char *s1, char *s2);
+char				*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
